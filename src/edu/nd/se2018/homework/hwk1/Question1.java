8dab4cffc9d0e055d801184723d61e24e4ed18ab
Chris Wall <Chris Wall@LAPTOP-JG0P250U>
2018-09-03 00:37:32 -0400

    Question1 commit

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,24 @@
 package edu.nd.se2018.homework.hwk1;
+import java.util.*;
+
 
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		int total=0;
+		HashSet<Integer> hNumbers = new HashSet<Integer>();
+		for (int num : numbers) {
+			hNumbers.add(num);
+		}
+		//for (int i=0; i<hNumbers.size(); i++) {
+		Iterator<Integer> i = hNumbers.iterator();
+		while(i.hasNext())	{
+			total=total+i.next();
+			//System.out.println(total);
+		}
+		System.out.println(total);
+		return total;
 	}
 }
