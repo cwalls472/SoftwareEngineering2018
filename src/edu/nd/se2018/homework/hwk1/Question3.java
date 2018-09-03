950ffe0b38529b06f9e7efb1ff8aa4d760bc9f99
Chris Wall <Chris Wall@LAPTOP-JG0P250U>
2018-09-03 04:41:30 -0400

    Question 3 commit

--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -4,6 +4,31 @@ public class Question3 {
 	public Question3(){}	
 	
     public int getMirrorCount(int[] numbers){
-		return 0;	
+		//if (numbers.length == 0) {
+			//return 0;
+		//}
+    	int[] mirrorNumbers = new int[numbers.length];
+    	for (int i = numbers.length - 1;i>=0; i--) {
+    		mirrorNumbers[numbers.length -1 -i] = numbers[i];
+    	}
+    	int longestSeq = 0;
+    	int currentSeq = 0;
+    	for (int i = numbers.length - 1;i>=0; i--) {
+    		if(numbers[i]==mirrorNumbers[i]) {
+    			System.out.println("In compare");
+    			currentSeq++;
+    		}
+    		else {
+    			if (longestSeq<currentSeq) {
+    				longestSeq=currentSeq;
+    			}
+    			currentSeq=0;
+    		}
+    	}
+    	if (longestSeq<currentSeq) {
+			longestSeq=currentSeq;
+		}
+    	System.out.println(longestSeq);
+		return longestSeq;	
 	}
 }
